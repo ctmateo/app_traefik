ed025b965cb44f66a6a7da53532027207ffac215
Mateo <eliermateoramirez@gmail.com>
2025-09-22 15:26:17 -0500

    Solve router local

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 const express = require('express');
-const bodyParser= require('body-parser');
+const app = express(); 
 
-const app = express();
-const port = 3000;
-
-// INTERMEDIOS 
-app.use(bodyParser.urlencoded({extended : true}))
-
-// URL GET DATA
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { 
     res.send(`<form action ="/message" method="post">
         <input type="text" name="value" placeholder="variable a enviar"/>
         <button type="submit">Enviar variable</button>
         </form>`);
 });
 
-// POST DATA FROM INPUT
+
 app.post('/message', (req, res) => {
     const variable = req.body.value;
     res.send(`La variable enviada es: ${variable}`);
@@ -23,9 +16,15 @@ app.post('/message', (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`Escuchando en el puerto ${port}`);
+    console.log(`Devolviendo al puerto ${port}`);
 });
 
+
+
+
+
+
+/// TRABAJO
 /* DOCKER COMPOSE -> CREAR UNA NUEVA APLICACION -> SUBIR IMAGEN -> PONER NOMBRE A LA APLICACION -> SELECCIONAR IMAGEN -> CONFIGURAR PUERTO 3000 -> CREAR APLICACION
 UTILIZAR TRAFIC
 Y UTILIZAR 2 INSTANCIAS DE LA APLICACION Y DEVOLVER AL USUARIO 
